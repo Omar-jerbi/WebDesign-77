513021eb1496a38782963a45e2e2a2dc7b8e9f99
Omar-jerbi <Omarjerbi98@gmail.com>
2022-05-15 11:38:22 +0200

    img unige OK

--- progetto01/public/js/dark_light.js
+++ progetto01/public/js/dark_light.js
@@ -2,8 +2,12 @@ function dark_lightFN(){
     var body = document.querySelector('body');
     if(body.classList.contains('dark-mode')){
       body.classList.remove('dark-mode');
+      var icon = document.querySelector('#DLIcon');
+      icon.innerHTML = "brightness_5";
     }else{
       body.classList.add('dark-mode');
+      var icon = document.querySelector('#DLIcon');
+      icon.innerHTML = "brightness_7";
     }
 
   }
