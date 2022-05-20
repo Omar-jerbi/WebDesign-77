e8162b0a152e996de9ed6acb51af5255adef3fba
Omar-jerbi <Omarjerbi98@gmail.com>
2022-05-20 21:11:04 +0200

    sol dark cards

--- progetto01/public/js/dark_light.js
+++ progetto01/public/js/dark_light.js
@@ -2,10 +2,12 @@ function dark_lightFN(){
     var body = document.querySelector('body');
     if(body.classList.contains('dark-mode')){
       body.classList.remove('dark-mode');
+      body.style.backgroundColor = "#c4c3c4";//style>class
       var icon = document.querySelector('#DLIcon');
       icon.innerHTML = "brightness_5";
     }else{
       body.classList.add('dark-mode');
+      body.style.backgroundColor = "#0e1724";//style>class
       var icon = document.querySelector('#DLIcon');
       icon.innerHTML = "brightness_7";
     }
